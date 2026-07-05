a9e3b91c29ba97d39cae1dc985f9323b78d7aa54
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-05 17:40:48 -0400

    Add initial implementation of Longest Common Substring algorithm with detailed comments and examples

---
+++ dynamic_programming/longest_common_substring.py
@@ -0,0 +1,26 @@
+# ============================================================
+# Longest Common Substring
+# ============================================================
+#
+# The longest common substring is the longest matching part that
+# appears continuously in both strings.
+#
+# Continuous means the characters must be next to each other.
+#
+# Example:
+#
+#   str1 = "ABABC"
+#   str2 = "BABCA"
+#
+# Common substrings include:
+#
+#   "A"
+#   "B"
+#   "AB"
+#   "ABC"
+#   "BABC"
+#
+# The longest common substring is:
+#
+#   "BABC"
+#
