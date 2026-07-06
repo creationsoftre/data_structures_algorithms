78b345a4476ed210fee114a6a4f85b220d03ad70
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-06 01:01:40 -0400

    Add detailed example and explanation for Coin Change problem in greedy algorithm

--- algorithims/greedy_algorithm.py
+++ algorithims/greedy_algorithm.py
@@ -13,3 +13,40 @@
 #
 # But greedy does not always give the best answer for every problem.
 #
+# ------------------------------------------------------------
+# EXAMPLE: COIN CHANGE
+# ------------------------------------------------------------
+#
+# Problem:
+#
+#   Given an amount of money, use the fewest coins possible.
+#
+# Example:
+#
+#   amount = 87
+#
+# Using common U.S. coins:
+#
+#   quarter = 25
+#   dime    = 10
+#   nickel  = 5
+#   penny   = 1
+#
+# Greedy choice:
+#
+#   Always take the largest coin that does not go over the
+#   remaining amount.
+#
+# For 87:
+#
+#   Take 25, remaining 62
+#   Take 25, remaining 37
+#   Take 25, remaining 12
+#   Take 10, remaining 2
+#   Take 1, remaining 1
+#   Take 1, remaining 0
+#
+# Result:
+#
+#   [25, 25, 25, 10, 1, 1]
+#
