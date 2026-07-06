c72d326a07fedc4d7efa2a5430f70c0ffe9d7e66
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-06 01:13:23 -0400

    Add initial implementation of Fractional Knapsack problem with detailed explanation and example

---
+++ algorithims/greedy_algorithm/fractional_knapsack.py
@@ -0,0 +1,34 @@
+# ============================================================
+# Fractional Knapsack - Greedy Algorithm
+# ============================================================
+#
+# Fractional knapsack is a greedy problem.
+#
+# Problem:
+#
+#   We have a bag with a weight limit.
+#   We have items with a value and a weight.
+#   We want to get the most value possible.
+#
+# The important rule:
+#
+#   We are allowed to take part of an item.
+#
+# This is why it is called fractional knapsack.
+#
+# Example:
+#
+#   Item A:
+#       value = 60
+#       weight = 10
+#
+#   Item B:
+#       value = 100
+#       weight = 20
+#
+#   Item C:
+#       value = 120
+#       weight = 30
+#
+#   capacity = 50
+#
