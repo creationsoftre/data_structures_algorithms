af1315dc886127c7064ea7b75bf2b3c41a79d334
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-06 11:59:08 -0400

    Add detailed comments and examples for Run-Length Encoding implementation

--- algorithims/huffman_compression.py
+++ algorithims/huffman_compression.py
@@ -14,3 +14,38 @@
 #   If the same character repeats, store the character and the
 #   number of times it repeats.
 #
+# ------------------------------------------------------------
+# EXAMPLE
+# ------------------------------------------------------------
+#
+# Original string:
+#
+#   "AAAABBBCCDAA"
+#
+# Compressed string:
+#
+#   "A4B3C2D1A2"
+#
+# Meaning:
+#
+#   A4 -> A appears 4 times
+#   B3 -> B appears 3 times
+#   C2 -> C appears 2 times
+#   D1 -> D appears 1 time
+#   A2 -> A appears 2 times
+#
+# ------------------------------------------------------------
+# BIG O / SPEED
+# ------------------------------------------------------------
+#
+# Time Complexity:
+#   O(n)
+#
+# Speed:
+#   Fast.
+#
+# Why?
+#   We scan through the string one time.
+#
+#   n = number of characters in the string
+#
