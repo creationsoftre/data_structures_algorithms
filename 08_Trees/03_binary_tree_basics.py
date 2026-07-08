c8970304e518dd31444cb2fc7048b00eb00bdbf0
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-08 12:05:44 -0400

    Add binary tree rules and distinctions between binary trees and binary search trees

--- 08_Trees/03_binary_tree_basics.py
+++ 08_Trees/03_binary_tree_basics.py
@@ -23,3 +23,25 @@
 # D and E are children of B.
 # F is the right child of C.
 #
+# ------------------------------------------------------------
+# BINARY TREE RULE
+# ------------------------------------------------------------
+#
+# A binary tree does not have to be sorted.
+#
+# The only rule is:
+#
+#   Each node can have at most two children.
+#
+# This is different from a Binary Search Tree.
+#
+# Binary Tree:
+#
+#   At most two children.
+#
+# Binary Search Tree:
+#
+#   At most two children,
+#   AND smaller values go left,
+#   AND larger values go right.
+#
