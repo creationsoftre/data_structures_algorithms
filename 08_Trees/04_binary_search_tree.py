918b18e580525edbec886da4a838f87d9de05360
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-08 12:22:51 -0400

    Add binary search tree basics with example and rules

---
+++ 08_Trees/04_binary_search_tree.py
@@ -0,0 +1,40 @@
+# ============================================================
+# Binary Search Tree Basics
+# ============================================================
+#
+# A Binary Search Tree is also called a BST.
+#
+# A BST is a special type of binary tree.
+#
+# Binary tree rule:
+#
+#   Each node has at most two children.
+#
+# BST rule:
+#
+#   Smaller values go to the left.
+#   Larger values go to the right.
+#
+# ------------------------------------------------------------
+# SIMPLE BST EXAMPLE
+# ------------------------------------------------------------
+#
+#              10
+#            /    \
+#           5      15
+#          / \    /  \
+#         2   7  12   20
+#
+# For node 10:
+#
+#   Values smaller than 10 go left.
+#   Values larger than 10 go right.
+#
+# Left side of 10:
+#
+#   5, 2, 7
+#
+# Right side of 10:
+#
+#   15, 12, 20
+#
