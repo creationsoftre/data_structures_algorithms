ed8a2f92972fc0b9b3c64a98e8948c7f2ebb5f03
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-08 00:28:29 -0400

    Add formula and explanation for Quadratic Probing collision handling in Hash Table

--- hashtable/quadratic_probing.py
+++ hashtable/quadratic_probing.py
@@ -11,3 +11,19 @@
 #   Linear probing checks the next spot one at a time.
 #   Quadratic probing jumps farther each time.
 #
+# ------------------------------------------------------------
+# FORMULA
+# ------------------------------------------------------------
+#
+# Starting index:
+#
+#   hash_index = key % table_size
+#
+# If there is a collision:
+#
+#   index = (hash_index + i^2) % table_size
+#
+# where:
+#
+#   i = 0, 1, 2, 3, ...
+#
