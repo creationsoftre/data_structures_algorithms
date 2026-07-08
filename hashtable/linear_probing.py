61f73885a2e9eba18d161973c10f8593eb2deb40
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-08 00:16:06 -0400

    Add initial implementation of Linear Probing collision handling in Hash Table

---
+++ hashtable/linear_probing.py
@@ -0,0 +1,11 @@
+# ============================================================
+# Hash Table - Linear Probing
+# ============================================================
+#
+# Linear probing is a collision-handling technique.
+#
+# A collision happens when two keys calculate the same index.
+#
+# Linear probing handles collisions by checking the next index
+# one spot at a time until it finds an empty spot.
+#
