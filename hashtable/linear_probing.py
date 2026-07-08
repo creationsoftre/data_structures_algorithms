ed922b4db8f13c07ebcfdca90f34fc7fff9984cc
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-08 00:17:07 -0400

    Add detailed formula and example for Linear Probing collision handling in Hash Table

--- hashtable/linear_probing.py
+++ hashtable/linear_probing.py
@@ -9,3 +9,44 @@
 # Linear probing handles collisions by checking the next index
 # one spot at a time until it finds an empty spot.
 #
+# ------------------------------------------------------------
+# FORMULA
+# ------------------------------------------------------------
+#
+# Starting index:
+#
+#   hash_index = key % table_size
+#
+# If there is a collision:
+#
+#   index = (hash_index + i) % table_size
+#
+# where:
+#
+#   i = 0, 1, 2, 3, ...
+#
+# ------------------------------------------------------------
+# EXAMPLE
+# ------------------------------------------------------------
+#
+# table_size = 10
+#
+# Insert key 20:
+#
+#   20 % 10 = 0
+#
+# Store key 20 at index 0.
+#
+# Insert key 30:
+#
+#   30 % 10 = 0
+#
+# Index 0 is already used.
+#
+# Try the next index:
+#
+#   index = (0 + 1) % 10
+#   index = 1
+#
+# Store key 30 at index 1.
+#
