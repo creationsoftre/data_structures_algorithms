a862a78564148630de06a8559be903b7b39a9bf4
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-08 10:37:44 -0400

    Add common hash functions documentation

---
+++ 07_hashtable/09_common_hash_functions.py
@@ -0,0 +1,25 @@
+# ============================================================
+# Common Hash Functions
+# ============================================================
+#
+# A hash function converts a key into a number.
+#
+# That number is then used to find an index in a hash table.
+#
+# Simple idea:
+#
+#   key -> hash function -> index
+#
+# Different key types need different hash functions.
+#
+# Examples:
+#
+#   Integer key:
+#       1023
+#
+#   String key:
+#       "CAT"
+#
+#   Username key:
+#       "trevonte"
+#
