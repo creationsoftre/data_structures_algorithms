fd8309e66ce1873bc42c12ae1be68bb9388cb87c
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-07 23:35:31 -0400

    Add initial implementation of Map ADT with key-value pair documentation

---
+++ hashtable/map_adt.py
@@ -0,0 +1,26 @@
+# ============================================================
+# Map ADT
+# ============================================================
+#
+# ADT means Abstract Data Type.
+#
+# A Map ADT stores key-value pairs.
+#
+# Simple idea:
+#
+#   Use a key to find a value.
+#
+# Examples:
+#
+#   student_id -> student_name
+#   username   -> email
+#   word       -> definition
+#
+# A key should be unique.
+#
+# That means:
+#
+#   One key maps to one value.
+#
+# If we add the same key again, we update the value.
+#
