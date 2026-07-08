b4b60ba45bae613a5500184d6db4ad46951582bf
Trevonte Wigfall <Trevontejw@gmail.com>
2026-07-08 00:31:23 -0400

    Add detailed example for Quadratic Probing collision handling in Hash Table

--- hashtable/quadratic_probing.py
+++ hashtable/quadratic_probing.py
@@ -27,3 +27,48 @@
 #
 #   i = 0, 1, 2, 3, ...
 #
+# ------------------------------------------------------------
+# EXAMPLE
+# ------------------------------------------------------------
+#
+# table_size = 11
+#
+# Insert key 22:
+#
+#   22 % 11 = 0
+#
+# Store key 22 at index 0.
+#
+# Insert key 33:
+#
+#   33 % 11 = 0
+#
+# Index 0 is already used.
+#
+# Try i = 1:
+#
+#   index = (0 + 1^2) % 11
+#   index = 1
+#
+# Store key 33 at index 1.
+#
+# Insert key 44:
+#
+#   44 % 11 = 0
+#
+# Index 0 is already used.
+#
+# Try i = 1:
+#
+#   index = (0 + 1^2) % 11
+#   index = 1
+#
+# Index 1 is already used.
+#
+# Try i = 2:
+#
+#   index = (0 + 2^2) % 11
+#   index = 4
+#
+# Store key 44 at index 4.
+#
